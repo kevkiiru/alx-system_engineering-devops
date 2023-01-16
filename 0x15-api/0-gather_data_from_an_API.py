c2ce83a470eb5b94b49a8b559a5ddfe19a67158c
kevkiiru <kevkiiru6@gmail.com>
2023-01-16 22:55:35 +0300

    gather data file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,7 +7,7 @@ import sys
 
 
 if __name__ == "__main__":
-    
+
     url = "https://jsonplaceholder.typicode.com/"
     user = requests.get(url + "users/{}".format(sys.argv[1])).json()
     todos = requests.get(url + "todos", params={"userId": sys.argv[1]}).json()
