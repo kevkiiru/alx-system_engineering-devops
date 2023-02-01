bf096c7e3bd9cd60164421a989d8ac819e3ac8a3
kevkiiru <kevkiiru6@gmail.com>
2023-02-01 22:21:49 +0300

    strace is your friend pp file

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,6 @@
+# fixes an error with a wordpress website
+
+exec { '/var/www/html/wp-setting.php':
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin'
+}
