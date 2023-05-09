725cd8443b3a53fab2809ce200ad92a09c5688da
kevkiiru <kevkiiru6@gmail.com>
2023-05-09 17:37:36 +0300

    subs py file

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -13,4 +13,4 @@ def number_of_subscribers(subreddit):
     response = requests.get(url_api, headers=headers)
     if response.status_code is not 200:
         return(0)
-    return(response.json().get("data").get("subscribers"))
+        return(response.json().get("data").get("subscribers"))
